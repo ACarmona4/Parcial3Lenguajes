27ea41ce8d2d8373697b0e5d7fb5dd9488a47dc1
ACarmona4 <124740790+ACarmona4@users.noreply.github.com>
2023-11-10 13:19:30 -0500

    Update Parcial3AlejandroCarmona.hs

--- Parcial3AlejandroCarmona.hs
+++ Parcial3AlejandroCarmona.hs
@@ -1,6 +1,6 @@
 -- Método que verifica si un número es primo
 verificarPrimo :: Int -> Bool
-verificarPrimo x = null [ n | n <- [2..x-1], x `mod` n == 0]
+verificarPrimo x = null [ n | n <- [2..x-1], (x `mod` n == 0)]
 
 -- Método que inserta en una lista los elementos en el rango ingresado que sean primos
 numerosEntreDosYelSiguienteValor :: Int -> [Int]
